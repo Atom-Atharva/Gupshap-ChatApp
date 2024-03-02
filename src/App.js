016ecf9b69932206d6b40a0b4033a40074d9a125
Atharva Sugandhi <atharva.sugandhi@gmail.com>
2024-03-02 21:56:51 +0530

    Login and SignUp Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-    return <div className="App text-3xl p-5">Hello World</div>;
-}
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
+const App = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />,
+        },
+        {
+            path: "/signup",
+            element: <SignUp />,
+        },
+    ]);
+
+    return (
+        <div className="font-chakraPetch">
+            <RouterProvider router={appRouter} />
+        </div>
+    );
+};
 
 export default App;
