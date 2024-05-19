611f400ec59847cbc0ce580fdc641fe6f274ba5b
atharva <atharva.sugandhi@gmail.com>
2024-05-19 17:59:14 +0530

    Handle Form SignUP, Added Hot-Toast

--- src/component/common/InputBox.js
+++ src/component/common/InputBox.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const InputBox = ({ placeholder, type }) => {
+const InputBox = ({ placeholder, value, type, onChange }) => {
     return (
         <div>
             <input
                 placeholder={placeholder}
                 type={type}
                 className="px-6 py-4 w-80 rounded border border-black border-opacity-20 font-semibold"
+                onChange={onChange}
+                value={value}
+                required
             />
         </div>
     );
--- src/component/signup/form/MyForm.js
+++ src/component/signup/form/MyForm.js
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import InputBox from "../../common/InputBox";
 import SignInUpBtn from "../../common/SignInUpBtn";
 import PictureDP from "./PictureDP";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 const MyForm = () => {
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [image, setImage] = useState(null);
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        console.log(image, name, email, password);
+
+        if (!image) {
+            toast.error("Profile Picture Needed!", {
+                position: "bottom-right",
+                style: {
+                    fontWeight: "600",
+                    background: "red",
+                    color: "white",
+                },
+                iconTheme: {
+                    primary: "white",
+                    secondary: "red",
+                },
+            });
+            return;
+        }
+
+        // Backend Handle
+
+        navigate("/chat");
+    };
+
     return (
-        <div className="flex flex-col gap-6 items-center">
-            <PictureDP />
-            <InputBox placeholder="Name" type="text" />
-            <InputBox placeholder="Email" type="email" />
-            <InputBox placeholder="Password" type="password" />
-            <SignInUpBtn signIn={false} />
-        </div>
+        <form
+            className="flex flex-col gap-6 items-center"
+            onSubmit={handleSignUp}
+        >
+            <Toaster />
+            <PictureDP value={image} onChange={(e) => setImage(e)} />
+            <InputBox
+                name="Name"
+                placeholder="Name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <InputBox
+                name="Email"
+                placeholder="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <InputBox
+                name="Password"
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <SignInUpBtn signIn={false} type="submit" />
+        </form>
     );
 };
 
