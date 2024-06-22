67f2a43a590f969b7bda7dfc7e52809f512bb417
Raghav-kanungo <raghavkanungo065@gmail.com>
2024-06-22 23:07:40 +0530

    svg fix Co-authored-by: Atom-Atharva <atharva.sugandhi@gmail.com>

--- src/component/chat/groupPage/form/Addmembers.js
+++ src/component/chat/groupPage/form/Addmembers.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { multiChatData, newperson, deluser } from "./formData";
+import { multiChatData, newperson } from "./formData";
 import MemberItem from "./MemberItem";
 
 const Addmembers = ({ member, setMember }) => {
   return (
-    <div className="flex flex-col gap-4  items-start self-stretch px-9 w-full">
-      <div className="text-xl font-semibold">Add Members</div>
-      <div className="flex h-60 flex-col rounded-lg border border-black-opacity-40 overflow-y-auto items-start self-stretch">
+    <div className="flex flex-col gap-4  items-center w-full px-9">
+      <div className="text-xl w-1/2 font-semibold">Add Members</div>
+      <div className="flex h-60 flex-col rounded-lg border px-4 border-black-opacity-40 overflow-y-auto items-start w-1/2 ">
         {multiChatData.map((item, index) => (
           <MemberItem
             key={index}
@@ -14,7 +14,6 @@ const Addmembers = ({ member, setMember }) => {
             name={item.name}
             id={item.id}
             newperson={newperson}
-            deluser={deluser}
             member={member}
             setMember={setMember}
           />
--- src/component/chat/mainarea/SearchArea.js
+++ src/component/chat/mainarea/SearchArea.js
@@ -10,8 +10,8 @@ const SearchArea = ({ Search }) => {
         className=" py-3 px-2 w-72 rounded border border-black border-opacity-20 text-base font-regular"
         required
       />
-      <button className="flex w-16 h-12 justify-center items-center gap-3 bg-secondary">
-        <img src={searchbtn} alt="Search" />
+      <button className="flex w-16 h-12 justify-center items-center gap-3 bg-secondary hover:bg-primary-btn-hover">
+        <img src={searchbtn} alt="Search" className="w-6 " />
       </button>
     </div>
   );
