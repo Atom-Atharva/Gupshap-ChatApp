3f58355a4307dede084341700517f393b64deab8
Raghav-kanungo <raghavkanungo065@gmail.com>
2024-06-28 01:27:14 +0530

    Add member api in group

--- src/component/chat/room/group-info/GroupInfo.js
+++ src/component/chat/room/group-info/GroupInfo.js
@@ -11,165 +11,166 @@ import { toggleChatObject } from "../../../../utils/redux/pagesSlice";
 import UsersList from "./UsersList";
 
 const GroupInfo = ({
-    info,
-    setGroupInfoVisible,
-    groupInfoVisible,
-    setAddMemberVisible,
+  info,
+  setGroupInfoVisible,
+  groupInfoVisible,
+  setAddMemberVisible,
+  updateInfo,
+  setUpdateInfo,
 }) => {
-    const user = useSelector((store) => store.user);
-    const [isGroupAdmin, setIsGroupAdmin] = useState(false);
-    const chatId = useSelector((store) => store?.pages?.chatObject?._id);
-    const [creator, setCreator] = useState(null);
-    const [members, setMembers] = useState(null);
-    const [updateInfo, setUpdateInfo] = useState(false);
-    const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+  const [isGroupAdmin, setIsGroupAdmin] = useState(false);
+  const chatId = useSelector((store) => store?.pages?.chatObject?._id);
+  const [creator, setCreator] = useState(null);
+  const [members, setMembers] = useState(null);
 
-    const leaveHandler = async () => {
-        await axios
-            .delete(LEAVE_GROUP_API + info._id, {
-                withCredentials: true,
-                credentials: "include",
-            })
-            .then((response) => {
-                toast.success(response.data.message, {
-                    position: "bottom-right",
-                    style: {
-                        fontWeight: "600",
-                        background: "green",
-                        color: "white",
-                    },
-                    iconTheme: {
-                        primary: "white",
-                        secondary: "green",
-                    },
-                });
-                dispatch(toggleChatObject(null));
-            })
-            .catch((error) => {
-                console.log(error);
-                toast.error(error?.response?.data?.message, {
-                    position: "bottom-right",
-                    style: {
-                        fontWeight: "600",
-                        background: "red",
-                        color: "white",
-                    },
-                    iconTheme: {
-                        primary: "white",
-                        secondary: "red",
-                    },
-                });
-            });
-    };
+  const dispatch = useDispatch();
 
-    const getChatInfo = () => {
-        axios
-            .get(GET_CHAT_INFO + chatId, {
-                withCredentials: true,
-                credentials: "include",
-            })
-            .then((response) => {
-                // console.log(response);
-                setMembers(response?.data?.data?.members);
-                setCreator(response?.data?.data?.creator);
-                // console.log(response?.data?.data?.creator);
-                setIsGroupAdmin(
-                    response?.data?.data?.creator?._id === user?._id
-                );
-            })
-            .catch((error) => {
-                console.log(error);
-                toast.error(error?.response?.data?.message, {
-                    position: "bottom-right",
-                    style: {
-                        fontWeight: "600",
-                        background: "red",
-                        color: "white",
-                    },
-                    iconTheme: {
-                        primary: "white",
-                        secondary: "red",
-                    },
-                });
-            });
-    };
+  const leaveHandler = async () => {
+    await axios
+      .delete(LEAVE_GROUP_API + info._id, {
+        withCredentials: true,
+        credentials: "include",
+      })
+      .then((response) => {
+        toast.success(response.data.message, {
+          position: "bottom-right",
+          style: {
+            fontWeight: "600",
+            background: "green",
+            color: "white",
+          },
+          iconTheme: {
+            primary: "white",
+            secondary: "green",
+          },
+        });
+        dispatch(toggleChatObject(null));
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error?.response?.data?.message, {
+          position: "bottom-right",
+          style: {
+            fontWeight: "600",
+            background: "red",
+            color: "white",
+          },
+          iconTheme: {
+            primary: "white",
+            secondary: "red",
+          },
+        });
+      });
+  };
 
-    // GET GROUP INFO
-    useEffect(() => {
-        getChatInfo();
-    }, [updateInfo]);
+  const getChatInfo = () => {
+    axios
+      .get(GET_CHAT_INFO + chatId, {
+        withCredentials: true,
+        credentials: "include",
+      })
+      .then((response) => {
+        // console.log(response);
+        response.data.data.members.sort((a, b) => a.name.localeCompare(b.name));
+        setMembers(response?.data?.data?.members);
+        setCreator(response?.data?.data?.creator);
+        // console.log(response?.data?.data?.creator);
+        setIsGroupAdmin(response?.data?.data?.creator?._id === user?._id);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error?.response?.data?.message, {
+          position: "bottom-right",
+          style: {
+            fontWeight: "600",
+            background: "red",
+            color: "white",
+          },
+          iconTheme: {
+            primary: "white",
+            secondary: "red",
+          },
+        });
+      });
+  };
 
-    return (
-        <div className="absolute top-8 left-10 bg-white flex flex-col max-h-[95vh] w-96 gap-4 p-5 border border-black border-opacity-20 rounded-lg shadow-lg">
-            <Toaster />
-            <div
-                className="absolute top-4 right-4 cursor-pointer"
-                onClick={() => setGroupInfoVisible(!groupInfoVisible)}
-            >
-                <img src={group.cut} className="h-6" alt="X" />
-            </div>
-            <div className="flex flex-col gap-3 justify-center">
-                <div className="flex justify-center overflow-hidden rounded-full">
-                    <img
-                        src={info.avatar.url}
-                        className="h-32 w-32 rounded-full"
-                        alt="DP"
-                    />
-                </div>
+  // GET GROUP INFO
+  useEffect(() => {
+    getChatInfo();
+  }, [updateInfo]);
 
-                <p className="text-center font-bold text-2xl">{info.name}</p>
-            </div>
-            <div className="flex gap-3 items-center">
-                <p className="font-semibold text-lg">
-                    {groupContaints.createdByHeading}
-                </p>
-                <p className="font-semibold">{creator?.name}</p>
-            </div>
-            <div className="mr-1">
-                <p className="font-semibold text-lg">
-                    {groupContaints.groupMembersHeading}
-                </p>
-                {isGroupAdmin && (
-                    <div
-                        className="cursor-pointer"
-                        onClick={() => {
-                            setAddMemberVisible(true);
-                        }}
-                    >
-                        <AddUsers />
-                    </div>
-                )}
-                <div className="h-56 overflow-auto">
-                    {members ? (
-                        members.map((member) => {
-                            return (
-                                <Member
-                                    key={member._id}
-                                    info={member}
-                                    isGroupAdmin={isGroupAdmin}
-                                    setMembers={setMembers}
-                                    setUpdateInfo={setUpdateInfo}
-                                    updateInfo={updateInfo}
-                                />
-                            );
-                        })
-                    ) : (
-                        <div className="h-56 overflow-auto font-semibold w-full text-center">
-                            Loading...
-                        </div>
-                    )}
-                </div>
-            </div>
-            <div className="flex justify-center border-0 border-t-2 pt-2">
-                <button
-                    className="w-11/12  py-2 rounded-lg bg-error hover:bg-error-hover text-white font-semibold"
-                    onClick={leaveHandler}
-                >
-                    {groupContaints.leaveGroup}
-                </button>
-            </div>
+  return (
+    <div className="absolute top-8 left-10 bg-white flex flex-col max-h-[95vh] w-96 gap-4 p-5 border border-black border-opacity-20 rounded-lg shadow-lg">
+      <Toaster />
+      <div
+        className="absolute top-4 right-4 cursor-pointer"
+        onClick={() => setGroupInfoVisible(!groupInfoVisible)}
+      >
+        <img src={group.cut} className="h-6" alt="X" />
+      </div>
+      <div className="flex flex-col gap-3 justify-center">
+        <div className="flex justify-center overflow-hidden rounded-full">
+          <img
+            src={info.avatar.url}
+            className="h-32 w-32 rounded-full"
+            alt="DP"
+          />
         </div>
-    );
+
+        <p className="text-center font-bold text-2xl">{info.name}</p>
+      </div>
+      <div className="flex gap-3 items-center">
+        <p className="font-semibold text-lg">
+          {groupContaints.createdByHeading}
+        </p>
+        <p className="font-semibold">{creator?.name}</p>
+      </div>
+      <div className="mr-1">
+        <p className="font-semibold text-lg">
+          {groupContaints.groupMembersHeading}
+        </p>
+        {isGroupAdmin && (
+          <div
+            className="cursor-pointer"
+            onClick={() => {
+              setAddMemberVisible(true);
+            }}
+          >
+            <AddUsers />
+          </div>
+        )}
+        <div className="h-56 overflow-auto">
+          {members ? (
+            members.map((member) => {
+              return (
+                <Member
+                  key={member._id}
+                  info={member}
+                  isGroupAdmin={isGroupAdmin}
+                  setMembers={setMembers}
+                  setUpdateInfo={setUpdateInfo}
+                  updateInfo={updateInfo}
+                />
+              );
+            })
+          ) : (
+            <div className="h-56 overflow-auto font-semibold w-full text-center">
+              Loading...
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex justify-center border-0 border-t-2 pt-2">
+        <button
+          className="w-11/12  py-2 rounded-lg bg-error hover:bg-error-hover text-white font-semibold"
+          onClick={leaveHandler}
+        >
+          {groupContaints.leaveGroup}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default GroupInfo;
--- src/component/chat/room/group-info/List.js
+++ src/component/chat/room/group-info/List.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Friend from "./Friend";
 
-const List = ({ friends }) => {
-    return friends.length === 0 ? (
-        <div className="flex justify-center items-center h-full w-full font-semibold opacity-40">
-            Your Friends Will Appear Here!
-        </div>
-    ) : (
-        <div className="flex flex-col h-full overflow-auto">
-            {friends.map((friend) => (
-                <Friend info={friend} />
-            ))}
-        </div>
-    );
+const List = ({ friends, updateInfo, setUpdateInfo }) => {
+  return friends.length === 0 ? (
+    <div className="flex justify-center items-center h-full w-full font-semibold opacity-40">
+      Your Friends Will Appear Here!
+    </div>
+  ) : (
+    <div className="flex flex-col h-full overflow-auto">
+      {friends.map((friend) => (
+        <Friend
+          info={friend}
+          updateInfo={updateInfo}
+          setUpdateInfo={setUpdateInfo}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default List;
