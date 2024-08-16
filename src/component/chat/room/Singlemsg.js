6e41412d79956f3d1aa0b41b99df0fa84b86b214
Raghav-kanungo <raghavkanungo065@gmail.com>
2024-08-16 22:22:00 +0530

    Some fixes

--- src/component/chat/room/Singlemsg.js
+++ src/component/chat/room/Singlemsg.js
@@ -3,23 +3,23 @@ import MessageBubble from "./MessageBubble";
 import { useSelector } from "react-redux";
 
 const Singlemsg = ({ message, isGroup }) => {
-    const user = useSelector((store) => store.user);
-    const isSenderUser = message?.sender?.name === user?.name;
-    // console.log(message.sender.name, user.name);
+  const user = useSelector((store) => store.user);
+  const isSenderUser = message?.sender?._id === user?._id;
+  // console.log(message.sender.name, user.name);
 
-    return (
-        <div
-            className={`${
-                isSenderUser ? "justify-end" : "justify-start"
-            } flex items-center`}
-        >
-            <MessageBubble
-                message={message}
-                isSenderUser={isSenderUser}
-                isGroup={isGroup}
-            />
-        </div>
-    );
+  return (
+    <div
+      className={`${
+        isSenderUser ? "justify-end" : "justify-start"
+      } flex items-center`}
+    >
+      <MessageBubble
+        message={message}
+        isSenderUser={isSenderUser}
+        isGroup={isGroup}
+      />
+    </div>
+  );
 };
 
 export default Singlemsg;
